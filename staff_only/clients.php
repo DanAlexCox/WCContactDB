5d5aa4d9c4ce50aad328ee11a308b314387df9d0
DanAlexCox <adala738@gmail.com>
2024-11-18 12:47:04 +0000

    client page made

---
+++ staff_only/clients.php
@@ -0,0 +1,21 @@
+<html>
+    <?php
+    //check session data
+    ?>
+    <head>
+        <title>Client</title>
+    </head>
+    <body>
+        <?php
+        include "navbar.php";
+        //Tasks:
+        //- Locate client table
+        //- View client table (later make specific permissions for specially authorized users)
+        //- Contact client
+        //(all client modifications can only be done with proper authorization)
+        //- Add client details
+        //- Modify client details
+        //- Remove client
+        ?>
+    </body>
+</html>
