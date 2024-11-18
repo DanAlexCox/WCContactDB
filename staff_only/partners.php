c2d0883690a6187fb7ab8dd93ae0f717f7aa67c0
DanAlexCox <adala738@gmail.com>
2024-11-18 12:47:12 +0000

    partners page made

---
+++ staff_only/partners.php
@@ -0,0 +1,21 @@
+<html>
+    <?php
+    //check session data
+    ?>
+    <head>
+        <title>Partners</title>
+    </head>
+    <body>
+        <?php
+        include "navbar.php";
+        //Tasks:
+        //- Locate Partnership table
+        //- View partnership table (later make specific permissions for specially authorized users)
+        //- Contact partners
+        //(all partner modifications can only be done with proper authorization)
+        //- Add partner details
+        //- Modify partner details
+        //- Remove partner
+        ?>
+    </body>
+</html>
