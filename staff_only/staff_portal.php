5fbf366fee31c1f8b7d1262638a31992a2296ba5
DanAlexCox <adala738@gmail.com>
2024-11-18 12:46:53 +0000

    staff main portal made

---
+++ staff_only/staff_portal.php
@@ -0,0 +1,15 @@
+<html>
+    <?php
+    //check session data
+    ?>
+    <head>
+        <title> Welcome ...</title>
+    </head>
+    <body>
+        <?php
+        include "navbar.php";
+        ?>
+        <a href="partners.php"><div id="partners"><h2>Partners</h2></div></a>
+        <a href="clients.php"><div id="clients"><h2>Clients</div></a>
+    </body>
+</html>
