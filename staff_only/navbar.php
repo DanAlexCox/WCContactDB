0850713791e93ea72102905d1943cda14b65e863
DanAlexCox <adala738@gmail.com>
2024-11-19 11:34:33 +0000

    turned navbar to sidebar

--- staff_only/navbar.php
+++ staff_only/navbar.php
@@ -1,8 +1,23 @@
 <link rel="stylesheet" type="text/css" href="CSS/navbar.css">
-<p>Hello Navbar</p>
-<?php
-if($_SERVER['REQUEST_URI'] === "/WCContactDB/staff_only/partners.php" or "/WCContactDB/staff_only/clients.php"){
-    echo "<li><a href='staff_portal.php'>Back to portal</a></li>";
-} 
-echo "<li><a href='logout.php'>Logout</a></li>";
-?>
+<section id="sidebar">
+          <a href="staff_portal.php" class="brand">
+            <span class="icon">
+                <img src="CSS/images/w-logo-blue.png" alt="womens consortium logo">
+            </span>
+          </a>
+          <ul class="side-menu top">
+            <?php
+            if($_SERVER['REQUEST_URI'] === "/WCContactDB/staff_only/staff_portal.php"){
+                echo "<li><a href='partners.php'><span class='text'>Partners</span></a></li>";
+                echo "<li><a href='clients.php'><span class='text'>Clients</span></a></li>";
+            } else{
+                echo "<li><a href='staff_portal.php'><span class='text'>Back to portal</span></a></li>";
+            }
+            ?>
+            <li>
+                <a href="logout.php">
+                    <span class="text">Logout</span>
+                </a>
+            </li>
+        </ul>
+    </section>
