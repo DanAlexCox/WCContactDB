4691d3cb8b4d8f22c3a2b62dcd1e1a390a98c7f0
DanAlexCox <adala738@gmail.com>
2024-11-19 11:35:44 +0000

    removed draft navbar

--- staff_only/staff_portal.php
+++ staff_only/staff_portal.php
@@ -11,7 +11,5 @@
         <?php
         include "navbar.php";
         ?>
-        <a href="partners.php"><div id="partners"><h2>Partners</h2></div></a>
-        <a href="clients.php"><div id="clients"><h2>Clients</div></a>
     </body>
 </html>
