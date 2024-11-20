a79f53c7facfbdf8ecb7c8fb38c7d87d34b52cf0
DanAlexCox <adala738@gmail.com>
2024-11-20 19:57:15 +0000

    begun to add custom email system

--- staff_only/partners.php
+++ staff_only/partners.php
@@ -1,3 +1,67 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require ('../../vendor/autoload.php');
+
+try {
+    //Create a new PHPMailer instance
+$mail=new PHPMailer();
+$mail->CharSet = 'UTF-8';
+
+//Tell PHPMailer to use SMTP
+$mail->IsSMTP();
+
+//Enable SMTP debugging
+//SMTP::DEBUG_OFF = off (for production use)
+//SMTP::DEBUG_CLIENT = client messages
+//SMTP::DEBUG_SERVER = client and server messages
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->Host = 'smtp.gmail.com';
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+$mail->Port = 465;
+$mail->SMTPAuth = true;
+
+$mail->Username = 'adala738@gmail.com';
+$mail->Password   = 'Space2017!';
+
+//Do not use user-submitted addresses in here
+$mail->setFrom('adala738@gmail.com', 'First Last');
+
+//$mail->AddReplyTo('no-reply@mycomp.com','no-reply');
+$mail->Subject    = 'PHPMailer gmail smtp test';
+
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+// $mail->msgHTML(file_get_contents('contents.html'), __DIR__);
+
+$mail->AddAddress('daniel@womensconsortium.org.uk', 'title1');
+//$mail->AddAddress('abc2@gmail.com', 'title2'); /* ... */
+
+$mail->Body = "Hello World";
+
+//Replace the plain text body with one created manually
+$mail->AltBody = 'Hello World part 2';
+
+//Attach an image file
+//$mail->addAttachment('images/phpmailer_mini.png');
+
+if(!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+    //Section 2: IMAP
+    //Uncomment these to save your message in the 'Sent Mail' folder.
+    #if (save_mail($mail)) {
+    #    echo "Message saved!";
+    #}
+}
+} catch(Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+?>
+
 <html>
     <?php
     //check session data
@@ -19,5 +83,7 @@
         //- Modify partner details
         //- Remove partner
         ?>
+
+
     </body>
 </html>
