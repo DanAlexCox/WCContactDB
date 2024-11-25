3eaeed69a091437788b3d23d60369b3035c56990
DanAlexCox <adala738@gmail.com>
2024-11-25 20:32:27 +0000

    added session features (if not logged in, username in title)

--- staff_only/staff_portal.php
+++ staff_only/staff_portal.php
@@ -1,9 +1,17 @@
 <html>
     <?php
     //check session data
+    session_start();
+    if(!isset($_SESSION['User_ID'])){
+        session_destroy();
+        header("Location: ../loginsystem/login.php");
+        exit();
+    }
     ?>
     <head>
-        <title> Welcome ...</title>
+        <?php
+        echo "<title> Welcome ".$_SESSION['username']."</title>";
+        ?>
         <link rel="stylesheet" type="text/css" href="CSS/main.css">
         <link rel="icon" type="image/x-icon" href="CSS/images/w-logo-blue.png">
     </head>
