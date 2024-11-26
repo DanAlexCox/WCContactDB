acfe17be9240d5917af08b8d63889a02d5b45d08
DanAlexCox <adala738@gmail.com>
2024-11-26 11:47:15 +0000

    temporary add create user to portal

--- staff_only/staff_portal.php
+++ staff_only/staff_portal.php
@@ -7,6 +7,21 @@
         header("Location: ../loginsystem/login.php");
         exit();
     }
+
+    if(isset($_GET['error'])){
+        // Sanitize the message to prevent XSS
+        $error = htmlspecialchars($_GET['error']);
+        // Display a JavaScript alert with the message
+        echo "<script>alert('$error');</script>";
+    }
+    
+    if(isset($_GET['msg'])){
+        // Sanitize the message to prevent XSS
+        $msg = htmlspecialchars($_GET['msg']);
+        // Display a JavaScript alert with the message
+        echo "<script>alert('$msg');</script>";
+    }
+    
     ?>
     <head>
         <?php
@@ -19,5 +34,14 @@
         <?php
         include "navbar.php";
         ?>
+
+<form method="post" action="registeruser.php">
+            <h2>Create User</h2>
+            <input type="email" placeholder="Email Address" name="new_email" required><br><br>
+            <input type="text" placeholder="Username" name="new_username" required><br><br>
+            <input type="password" placeholder="Password" name="new_password" required><br><br>
+            <button type="submit" name="registerbtn">Create</button>
+        </form>
+
     </body>
 </html>
