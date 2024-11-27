149b69f12acde613cc6ae7a68eee014afba405c0
DanAlexCox <adala738@gmail.com>
2024-11-27 17:11:57 +0000

    added partner remove functionality

--- staff_only/partners.php
+++ staff_only/partners.php
@@ -44,7 +44,7 @@ if(isset($_GET['msg'])){
 
         //- View partnership table (complete later make specific permissions for specially authorized users)
 
-            echo "<form method = 'post' action = 'partners.php' class = 'emlptnr'><table class = 'ptnr'>";
+            echo "<form method = 'post' action = 'partners.php' class = 'partnertable'><table class = 'ptnr'>";
             echo "<tr><th>Partner</th><th>Representative</th><th>Select</th></tr>";
             foreach($query as $row){
             //verify no dupes
@@ -55,52 +55,80 @@ if(isset($_GET['msg'])){
                 echo "<td>".$row['Contact']."</td>";
                 echo "<td><input type = 'checkbox' class = 'checkbox' name = 'email[]' value = '".$row['partner_email']."'></td>";
                 echo "<input type = 'hidden' name = 'contactname[]' value = '".$row['Partner']."'>";
+                echo "<input type = 'hidden' name = 'partnerid' value = '".$row['partner_ID']."'>";
                 echo "</tr>";
                 array_push($rowset, $row);
             }
             }
             echo "</table>";
-            echo "<input type='submit' class='mkemlbtn' value='Contact'>";
+
+            echo "<input type='submit' class='dletbtn' name='dletbtn' value='Delete'>";
+            echo "<input type='submit' class='mkemlbtn' name='mkemlbtn' value='Contact'>";
             echo "</form>";
 
         //- Contact partners (complete)
             if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'])) {
-                echo "<div class='pcontact'>";
+                if(isset($_POST['mkemlbtn'])){
+                    echo "<div class='pcontact'>";
         
         // Display the email form (complete)
-            echo "<form id='emailForm' method='post' onsubmit='return checkPassword();' action='sendemail.php' class='sndeml'>";
+                    echo "<form id='emailForm' method='post' onsubmit='return checkPassword();' action='sendemail.php' class='sndeml'>";
         // Loop through selected checkboxes (complete)
-            $buttons = array();
-            foreach ($_POST['email'] as $partnerEM) {
-                echo "<input type='hidden' name='selected_emails[]' value='" . htmlspecialchars($partnerEM) . "'>";
-                array_push($buttons, $partnerEM);
-            }
-            $buttonsString = implode(", ", $buttons);
+                    $buttons = array();
+                    foreach ($_POST['email'] as $partnerEM) {
+                        echo "<input type='hidden' name='selected_emails[]' value='" . htmlspecialchars($partnerEM) . "'>";
+                        array_push($buttons, $partnerEM);
+                    }
+                    $buttonsString = implode(", ", $buttons);
 
-            $part = array();
-            foreach($_POST['contactname'] as $contactNM){
-                echo "<input type = 'hidden' name = 'selected_contacts[]' value = '".htmlspecialchars($contactNM)."'>";
-                array_push($part, $contactNM);
-            }
-            $partString = implode(", ", $part);
+                    $part = array();
+                    foreach($_POST['contactname'] as $contactNM){
+                        echo "<input type = 'hidden' name = 'selected_contacts[]' value = '".htmlspecialchars($contactNM)."'>";
+                        array_push($part, $contactNM);
+                    }
+                    $partString = implode(", ", $part);
 
-            echo "<label for class='email'>Email</label><br>";
-            echo "<input type = 'text' class = 'email' name ='emails[]' value = '".htmlspecialchars($buttonsString)."' readonly><br>";
-            echo "<label for class='title'>Title</label><br>";
-            echo "<input type = 'text' class = 'title' name = 'title' placeholder = 'Insert title' required><br>";
-            echo "<label for class='description'>Description</label><br>";
-            echo "<input type = 'text' class = 'description' name = 'description' placeholder = 'Insert details here'><br>";
-            echo "<input type = 'hidden' name = 'contacts[]' value = ". htmlspecialchars($partString) .">";
-            echo "<input type='hidden' name='sndemlbtn' value='1'>";
-            echo "<button type='submit'>Send</button>";
-            echo "</form>";
+                    echo "<label for class='email'>Email</label><br>";
+                    echo "<input type = 'text' class = 'email' name ='emails[]' value = '".htmlspecialchars($buttonsString)."' readonly><br>";
+                    echo "<label for class='title'>Title</label><br>";
+                    echo "<input type = 'text' class = 'title' name = 'title' placeholder = 'Insert title' required><br>";
+                    echo "<label for class='description'>Description</label><br>";
+                    echo "<input type = 'text' class = 'description' name = 'description' placeholder = 'Insert details here'><br>";
+                    echo "<input type = 'hidden' name = 'contacts[]' value = ". htmlspecialchars($partString) .">";
+                    echo "<input type='hidden' name='sndemlbtn' value='1'>";
+                    echo "<button type='submit'>Send</button>";
+                    echo "</form>";
+                    echo "</div>";
+                }
+        //- Remove partner (complete)
+                if(isset($_POST['dletbtn'])){
+                    $partnerid = htmlspecialchars($_POST['partnerid']);
+
+                    $delete_query = "DELETE FROM `partners` WHERE partner_ID = :id";
+                    $delete_stmt = $pdo->prepare($delete_query);
+                    $delete_stmt->bindParam(':id', $partnerid);
+                    if($delete_stmt->execute()){
+                        $msg = "Successfully deleted.";
+                        header("Location: partners.php?msg=".$msg);
+                        exit();
+                    } else{
+                        $error = "Unable to delete.";
+                        header("Location: partners.php?error=".$error);
+                        exit();
+                    }
+                } else{
+                    $error = "No valid input detected.";
+                    header("Location: partners.php?error=".$error);
+                    exit();
+                }
+
             }
-        //(later all partner modifications can only be done with proper authorization)
-        //- Add partner details
+        //!!!!(later all partner modifications (add, edit, remove) can only be done with proper authorization)!!!!
+        //- Add partner details (complete)
             echo "<a href='addpartner.php'><button>Add a partner</button></a>";
 
         //- Modify partner details
-        //- Remove partner
+        
         ?>
 
 
