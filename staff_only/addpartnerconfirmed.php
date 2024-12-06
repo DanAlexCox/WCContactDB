5a690629dc39157579d1227e55c8a89529331e99
DanAlexCox <adala738@gmail.com>
2024-12-06 14:44:25 +0000

    fixxed sql statement

--- staff_only/addpartnerconfirmed.php
+++ staff_only/addpartnerconfirmed.php
@@ -7,7 +7,7 @@ if(isset($_POST['regpartner'])){
     $newname = htmlspecialchars($_POST['Prefix']) . " " . htmlspecialchars($_POST['Forename']) . " " . htmlspecialchars($_POST['Surname']);
     $neweml = $_POST['EmailAddress'];
 
-    $ptnr_query = "INSERT `partners`(partner_name, address, representative, partner_email) VALUES (:pn, :ad, :rep, :pe)";
+    $ptnr_query = "INSERT `partners`(Partner_name, Address, Representative, Partner_email) VALUES (:pn, :ad, :rep, :pe)";
     $ptnr_stmt = $pdo->prepare($ptnr_query);
     $ptnr_stmt->bindParam(':pn', $neworg);
     $ptnr_stmt->bindParam(':ad', $newpc);
