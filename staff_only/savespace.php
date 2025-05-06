7680f79eca86c4d498e22f8a4ebd46ac1ade0caa
DanAlexCox <adala738@gmail.com>
2025-05-06 14:36:31 +0100

    hidden phpmailer text

---
+++ staff_only/savespace.php
@@ -0,0 +1,23 @@
+<?php
+
+        // $emailone = [];
+        // $emailtonameone = [];
+
+        // for ($i = 0; $i < count($emailArray); $i++) {
+        //     $emailone[] = $emailArray[$i];
+        //     $emailtonameone[] = $contactArray[$i];
+        // }
+
+        // foreach ($emailone as $index => $email) {
+            // $emailto = $email;
+            // $emailtoname = $emailtonameone[$index];
+
+
+            
+            // }
+        // unset($_SESSION['passgood']);
+        // $msg = 'Email sent!';
+        // header("Location: partners.php?msg=".urlencode($msg));
+        // ob_end_clean(); // Clear output buffer
+        // exit();
+?>
