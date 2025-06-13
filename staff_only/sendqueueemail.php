c263503b90350a938b270d0106302986028ba3c9
DanAlexCox <adala738@gmail.com>
2025-06-13 10:56:38 +0100

    added emailqueue functionality

--- staff_only/sendqueueemail.php
+++ staff_only/sendqueueemail.php
@@ -1,4 +1,58 @@
 <?php
+include "connectdb.php";
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require __DIR__.'/../vendor/autoload.php';
+
 //Select the latest 200 emails queued to be sent (test latest 1 email)
+$queueStmt = "SELECT `emailqueue`.*, `clients`.Email FROM `emailqueue`
+                INNER JOIN `clients` ON `clients`.Client_ID = `emailqueue`.Client_ID
+                WHERE EmailStatus_ID = 1 LIMIT 1";
+$queueSQL = $pdo->query($queueStmt);
+$queueSQL->execute();
 //foreach loop sending to queued email addresses
+foreach($queueSQL as $entry){
+    $queueID = $entry['Queue_ID'];
+    $emailto = $entry['Email'];
+    $emailtoname = $entry['Email'];
+    $emailfrom = "marketing@womensconsortium.org.uk";
+    $emailfromname = "WC Marketing";
+    $emailfrompass = "jkYd[uPLmxg|";
+    $title = $entry['Subject'];
+    $description = $entry['Body'];
+
+    $mail=new PHPMailer(true);
+    $mail->CharSet = 'UTF-8';
+    $mail->IsSMTP();
+    $mail->Host = 'ams203.greengeeks.net';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->SMTPDebug = 0;
+
+    $mail->Username = $emailfrom;
+    $mail->Password = $emailfrompass;
+
+    $mail->setFrom($emailfrom, $emailfromname);
+
+    $mail->Subject = $title;
+
+    $mail->AddAddress($emailto, $emailtoname);
+
+    $mail->Body = $description;
+    $mail->isHTML(true);
+    $mail->AltBody = $description;
+
+    if(!$mail->send()) {
+        error_log('Mailer Error: ' . $mail->ErrorInfo);
+    } else{
+        $updateQueueStmt = "UPDATE `emailqueue` SET EmailStatus_ID = 2 WHERE Queue_ID = $queueID";
+        $updateQueueSQL = $pdo->query($updateQueueStmt);
+        $updateQueueSQL->execute();
+    }
+    usleep(100000);
+}
 ?>
