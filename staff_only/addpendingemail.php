113dfa381e55d855192eb4451fd14cbcda73832b
DanAlexCox <adala738@gmail.com>
2025-06-18 19:57:24 +0100

    added import queue version

--- staff_only/addpendingemail.php
+++ staff_only/addpendingemail.php
@@ -3,7 +3,27 @@ include "connectdb.php";
 //Check subject and Layout_ID is selected to be used for emailqueue table
 if(isset($_POST['addpending'])){
     $subject = htmlspecialchars($_POST['title']);
-    $body = $_POST['description'];
+    if(isset($_POST['importfinish'])){
+        if(isset($_FILES['posterimport'])){
+            $uploadDir = 'uploads/';
+            $uploadNam = basename($_FILES['posterimport']['name']);
+            $uploadFile = $uploadDir.$uploadNam;
+
+            if (move_uploaded_file($_FILES['posterimport']['tmp_name'], $uploadFile)) {
+                $body = $uploadNam;
+            } else {
+                $error = "File upload failed.";
+                header("Location: promotion.php?error=".urlencode($error));
+                exit();
+            }
+        }
+    } elseif(isset($_POST['blankfinish'])){
+        $body = $_POST['description'];
+    } else{
+        $error = htmlspecialchars("Invalid finish.");
+        header("Location: promotion.php?error=".$error);
+        exit();
+    }
 
     //Select correct email type and receive client ids
     $emailType = $_POST['emailtype'];
