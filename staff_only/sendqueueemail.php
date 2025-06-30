472ef9b41320974061ce50fbb596870f7b114583
DanAlexCox <adala738@gmail.com>
2025-06-30 13:15:24 +0100

    fixed problem with sending to invalid clients

--- staff_only/sendqueueemail.php
+++ staff_only/sendqueueemail.php
@@ -14,8 +14,8 @@ $queueStmt = "SELECT `emailqueue`.*, `clients`.Email FROM `emailqueue`
 //Test with first queue id
 // $queueStmt = "SELECT `emailqueue`.*, `clients`.Email FROM `emailqueue`
 //                 INNER JOIN `clients` ON `clients`.Client_ID = `emailqueue`.Client_ID
-//                 WHERE Queue_ID = 1 LIMIT 1";
-$queueSQL = $pdo->query($queueStmt);
+//                 WHERE EmailStatus_ID = 1 LIMIT 6";
+$queueSQL = $pdo->prepare($queueStmt);
 $queueSQL->execute();
 //foreach loop sending to queued email addresses
 foreach($queueSQL as $entry){
@@ -27,45 +27,56 @@ foreach($queueSQL as $entry){
     $emailfrompass = "jkYd[uPLmxg|";
     $title = $entry['Subject'];
 
-    $mail=new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->IsSMTP();
-    $mail->Host = 'ams203.greengeeks.net';
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = 587;
-    $mail->SMTPAuth = true;
-    $mail->SMTPDebug = 0;
-
-    $mail->Username = $emailfrom;
-    $mail->Password = $emailfrompass;
-
-    $mail->setFrom($emailfrom, $emailfromname);
-
-    $mail->Subject = $title;
-
-    $uploadDir = __DIR__ . '/uploads/';
-    $uploadNam = $entry['Body'];
-    $uploadFile = $uploadDir . $uploadNam;
-    if(is_file($uploadFile) && getimagesize($uploadFile)){
-        $description = $uploadNam;
-        $body = "<div id='emailbody'>
-                    <img src='cid:$uploadNam' alt='Poster Image' style='max-width: 70%;'>
-                </div>";
-        $mail->addEmbeddedImage($uploadFile, $uploadNam);
-    } else{
-        $description = $entry['Body'];
-        $body = $description;
-    }
-    $mail->AddAddress($emailto, $emailtoname);
-    
-    $mail->Body = $body;
-    $mail->isHTML(true);
-    $mail->AltBody = $description;
-
-    if(!$mail->send()) {
+    if(empty($emailto) || !filter_var($emailto, FILTER_VALIDATE_EMAIL)){
         error_log('Mailer Error: ' . $mail->ErrorInfo);
-    } else{
+        $updateQueueStmt = "UPDATE `emailqueue` SET EmailStatus_ID = 4 WHERE Queue_ID = $queueID";
+        $updateQueueSQL = $pdo->query($updateQueueStmt);
+        $updateQueueSQL->execute();
+        continue;
+    }
+
+    try{
+        $mail=new PHPMailer(true);
+        $mail->CharSet = 'UTF-8';
+        $mail->IsSMTP();
+        $mail->Host = 'ams203.greengeeks.net';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+        $mail->Port = 587;
+        $mail->SMTPAuth = true;
+        $mail->SMTPDebug = 0;
+
+        $mail->Username = $emailfrom;
+        $mail->Password = $emailfrompass;
+
+        $mail->setFrom($emailfrom, $emailfromname);
+
+        $mail->Subject = $title;
+
+        $uploadDir = __DIR__ . '/uploads/';
+        $uploadNam = $entry['Body'];
+        $uploadFile = $uploadDir . $uploadNam;
+        if(is_file($uploadFile) && getimagesize($uploadFile)){
+            $description = $uploadNam;
+            $body = "<div id='emailbody'>
+                        <img src='cid:$uploadNam' alt='Poster Image' style='max-width: 70%;'>
+                    </div>";
+            $mail->addEmbeddedImage($uploadFile, $uploadNam);
+        } else{
+            $description = $entry['Body'];
+            $body = $description;
+        }
+        $mail->AddAddress($emailto, $emailtoname);
+        $mail->Body = $body;
+        $mail->isHTML(true);
+        $mail->AltBody = $description;
+        $mail->send();
+
         $updateQueueStmt = "UPDATE `emailqueue` SET EmailStatus_ID = 2 WHERE Queue_ID = $queueID";
+        $updateQueueSQL = $pdo->query($updateQueueStmt);
+        $updateQueueSQL->execute();
+    } catch (Exception $e) {
+        error_log('Mailer Error: ' . $e->getMessage());
+        $updateQueueStmt = "UPDATE `emailqueue` SET EmailStatus_ID = 3 WHERE Queue_ID = $queueID";
         $updateQueueSQL = $pdo->query($updateQueueStmt);
         $updateQueueSQL->execute();
     }
